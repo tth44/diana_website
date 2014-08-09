3433026baa69c919156d8f0e6fba45bcdebdbcf9
tth44 <matthieu.desbois44@gmail.com>
2014-08-09 12:25:57 +0300

    a

--- diana_website/settings.py
+++ diana_website/settings.py
@@ -20,7 +20,8 @@ ALLOWED_HOSTS = ['*']
 
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
-DATABASES =  {'default' : dj_database_url.parse('postgres://mgrjxzwxtndxrg:M-sbpi4V5N7M0yTWx2Lijs3r1f@ec2-174-129-197-200.compute-1.amazonaws.com:5432/d3rpkqatvcc0s0') }
+DATABASES ={}
+DATABASES['default']= dj_database_url.config()
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
@@ -69,12 +70,12 @@ WSGI_APPLICATION = 'diana_website.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+#----------------------------------------------------------------- DATABASES = {
+    #-------------------------------------------------------------- 'default': {
+        #------------------------------- 'ENGINE': 'django.db.backends.sqlite3',
+        #------------------------- 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    #------------------------------------------------------------------------- }
+#----------------------------------------------------------------------------- }
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
